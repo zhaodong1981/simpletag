9afe1175cd576ed8ed636a66bd5e2882618d7606
Dong Zhao <dzhao@redhat.com>
2019-06-13 23:49:41 +0800

    used the refresh feature of material table.

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React, {Component} from 'react';
 import './App.css';
 import MaterialTable from 'material-table';
 import TagButton from './TagButtons';
-import RefreshData from './RefreshData';
 
 class App extends Component {
   constructor(props) {
@@ -11,7 +10,7 @@ class App extends Component {
     this.tableRef = React.createRef();
   }
   state = {
-    bookmarks: []
+    bookmarks : [],
   };
   
   componentDidMount() {
@@ -28,7 +27,8 @@ class App extends Component {
     fetch('http://tag.zhaodong.name/api/link')
     .then(res => res.json())
     .then((data) => {
-      this.setState({ bookmarks: data });
+      console.log(this.props.Mytable);
+     // this.props.Mytable.setState({ data: {data} });
       console.log(data);
     })
     .catch(console.log)
@@ -50,14 +50,37 @@ class App extends Component {
     return (
       
       <div className="App">
-      <RefreshData> </RefreshData>
       
-      <TagButton Refresh={this.refreshBookmarks} state={this.state.bookmarks}></TagButton>
+      <TagButton Refresh={this.refreshBookmarks}></TagButton>
       <MaterialTable
-      title="My bookmarks"
-      tableRef={this.tableRef}
-      columns={mycolumns}
-      data={this.state.bookmarks}
+        title="My bookmarks"
+        tableRef={this.tableRef}
+        columns={mycolumns}
+      //  data={mydata}
+        data={query =>
+          new Promise((resolve, reject) => {
+            let url = 'http://tag.zhaodong.name/api/link?'
+            url += 'per_page=' + query.pageSize
+            url += '&page=' + (query.page + 1)
+            fetch(url)
+              .then(response => response.json())
+              .then(result => {
+                resolve({
+                  data: result.data,
+                  page: result.page - 1,
+                  totalCount: result.total,
+                })
+              })
+          })
+        }
+        actions={[
+          {
+            icon: 'refresh',
+            tooltip: 'Refresh Data',
+            isFreeAction: true,
+            onClick: () => this.tableRef.current && this.tableRef.current.onQueryChange(),
+          }
+        ]}
     />
     </div>
     );
