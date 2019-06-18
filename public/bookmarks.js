d854357a90db75c33d3b543c6368c90f480f6506
Dong Zhao <dzhao@redhat.com>
2019-06-18 15:07:48 +0800

    update width of tag.html

--- public/bookmarks.js
+++ public/bookmarks.js
@@ -4,8 +4,6 @@ var mainApp = angular.module("mainApp", []);
 
 mainApp.controller('bookmarkController', function($scope, $http, $location) {
    var tag = $location.search().name;
-   
-   alert("tag = "+ tag);
    $scope.bookmarks = [];
    //only get the recently modified 50 bookmakrs
    $http.get('/api/link/search?tag='+tag).then(function (result) {
