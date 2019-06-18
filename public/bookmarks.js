1a15be9df43a1e4a01a6e3ebf9f6ced6d19fb19a
Dong Zhao <dzhao@redhat.com>
2019-06-18 15:01:35 +0800

    updated the page to show bookmarks of a tag

--- public/bookmarks.js
+++ public/bookmarks.js
@@ -1,15 +1,14 @@
 'use strict';
 var mainApp = angular.module("mainApp", []);
- 
-
 //display existing tags
-mainApp.controller('bookmarkController', function($scope, $http) {
-   var tag = $location.search().tag;
+
+mainApp.controller('bookmarkController', function($scope, $http, $location) {
+   var tag = $location.search().name;
    
    alert("tag = "+ tag);
    $scope.bookmarks = [];
    //only get the recently modified 50 bookmakrs
-   $http.get('/api/link?search?tag='+tag).then(function (result) {
+   $http.get('/api/link/search?tag='+tag).then(function (result) {
       $scope.bookmarks =result.data;
    });
    
