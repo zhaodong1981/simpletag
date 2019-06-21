a1c1520c7bb76b5f1de8457a0dc98fbe681d04b5
Dong Zhao <dzhao@redhat.com>
2019-06-21 14:06:17 +0800

    added jwt based token for tag.html

--- public/bookmarks.js
+++ public/bookmarks.js
@@ -8,7 +8,16 @@ mainApp.controller('bookmarkController', function($scope, $http, $location) {
       tag = encodeURIComponent(tag);
       $scope.bookmarks = [];
    //only get the recently modified 50 bookmakrs
-      $http.get('/api/link/search?tag='+tag).then(function (result) {
+      let user = JSON.parse(localStorage.getItem('user'));
+      let token = '';
+      if (user && user.token) {
+        token = user.token;
+      }
+      $http.get('/api/link/search?tag='+tag,
+      {
+         headers: {'Authorization': 'Bearer ' + token }
+      }
+      ).then(function (result) {
          $scope.bookmarks =result.data;
       });
    }
