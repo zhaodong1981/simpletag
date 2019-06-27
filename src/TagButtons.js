4280893921e1d776e7f923a10600cce117e0bbb2
Dong Zhao <dzhao@redhat.com>
2019-06-27 15:13:17 +0800

    removed useless code

--- src/TagButtons.js
+++ src/TagButtons.js
@@ -1,7 +1,6 @@
 import React,{Component}  from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
-import CreateIcon from '@material-ui/icons/Create';
 import RefreshIcon from '@material-ui/icons/Refresh';
 import { Link } from 'react-router-dom';
 
