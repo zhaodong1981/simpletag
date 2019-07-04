e2470467a8c0c07963c3a702b36b23f347a41108
Dong Zhao <dzhao@redhat.com>
2019-07-04 17:07:34 +0800

    reload when Enter key pressed

--- src/TagButtons.js
+++ src/TagButtons.js
@@ -10,12 +10,28 @@ class TagButtons extends Component {
   constructor(props){
     super(props);
     this.onSearch = this.onSearch.bind(this);
+    this.handleEnter = this.handleEnter.bind(this);
+    this.handleKeyPress = this.handleKeyPress.bind(this);
   }
   onSearch(e) {
     e.preventDefault();
-    this.props.Refresh(this.title.value);
+    this.props.Refresh(this.keywords.value);
   }
-
+  componentDidMount() {
+    document.addEventListener('keydown', this.handleKeyPress);
+  }
+  componentWillUnmount() {
+    document.removeEventListener('keydown', this.handleKeyPress);
+  }
+  // change code above this line
+  handleEnter() {
+    this.props.Refresh(this.keywords.value);
+  }
+  handleKeyPress(event) {
+    if (event.keyCode === 13) {
+      this.handleEnter();
+    }
+  }
 render() {
     const classes = makeStyles(theme => ({
         button: {
@@ -24,11 +40,11 @@ render() {
       }));
 return(
         <div>
-             <input type="text" className="App-keywords" ref={(c) => this.title = c} name="title" />
+             <input type="text" className="App-keywords" ref={(c) => this.keywords = c} name="keywords" />
       
             <Button variant="contained" color="primary" className={classes.button} onClick={this.onSearch}>
               <RefreshIcon className={classes.rightIcon} />
-              Search
+              Reload
             </Button>
             <Link to="/login/">Logout</Link>
         </div>
