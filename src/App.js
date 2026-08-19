93e41a75d36d4de7fd7a03e2b0511f484e9f9ef3
zhaodong@zgc <zhaodong@zgc.com>
2026-08-19 16:23:34 +0800

    fix path

--- src/App.js
+++ src/App.js
@@ -257,7 +257,7 @@ class App extends Component {
       { title: 'Date', field: 'modifydate'},
       { title: 'Tags', field: 'tags', render: rowData => 
       <div>{rowData.tags && rowData.tags.constructor === Array && rowData.tags.map((tag, index) => (
-          <a href={'/tag.html#?name='+tag} target="_blank" rel="noopener noreferrer" style={{marginRight: '10px'}}>{tag}</a>
+          <a href={'/tag/tag.html#?name='+tag} target="_blank" rel="noopener noreferrer" style={{marginRight: '10px'}}>{tag}</a>
         ))}
       </div>
       }
